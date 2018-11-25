944e2d10268c446f48f34f4da840190698743164
ink <ink@192.168.0.206>
2018-11-25 23:58:48 +0200

    new_repository

---
+++ Exercitii_afisare/src/Exercitiul1.java
@@ -0,0 +1,14 @@
+
+public class Exercitiul1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1");
+		System.out.println("22");
+		System.out.println("333");
+		System.out.println("4444");
+		System.out.println("55555");
+			
+	}
+
+}
---
+++ MJ_CURS_testGIT/src/MJ_TEST_GIT.java
@@ -0,0 +1,9 @@
+
+public class MJ_TEST_GIT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("test");
+	}
+
+}
